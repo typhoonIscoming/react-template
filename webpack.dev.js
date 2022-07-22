0887c7dc3613fbaa2092f47cd7719dfc4bf300a9
typhoonIscoming <850525344@qq.com>
2022-07-22 12:00:44 +0800

    启动时清除无用信息

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
 const { merge } = require('webpack-merge');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 const baseConfig = require('./webpack.config');
 const config = require('./config');
@@ -15,15 +16,24 @@ const options = {
     host: HOST,
     open: true,
     historyApiFallback: true,
+    stats: 'errors-only', // 加上这一行, 代表: 只打印错误日志
 };
 
 const webpackConfig = merge(baseConfig, {
     devServer: {
         disableHostCheck: true,
         https: false,
+        overlay: true, // 编译出现错误时，将错误直接显示在页面上
+        quiet: true, // 如果使用webpack-dev-server，需要设为true，禁止显示devServer的console信息
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
+        new FriendlyErrorsWebpackPlugin({
+            clearConsole: true,
+            compilationSuccessInfo: {
+                messages: [`Your application is running here: http://${HOST}:${PORT}`],
+            },
+        }),
     ],
 });
 
